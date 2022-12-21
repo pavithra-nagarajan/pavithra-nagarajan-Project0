c7b03b3e18f0007ddcef095bbc82f189debdd184
Keyne S Cleetus <58383657+keynecleetus@users.noreply.github.com>
2022-12-21 13:34:19 +0530

    Update Calculator.java

--- src/main/java/com/Calculator.java
+++ src/main/java/com/Calculator.java
@@ -1,10 +1,27 @@
 package com;
 
 public class Calculator {
-    public int evaluate(String expression) {
+    public int evaluateSum(int exp1,int exp2) {
         int sum = 0;
-        for (String summand : expression.split("\\+"))
-            sum += Integer.valueOf(summand);
+      
         return sum;
+    }
+
+    public int evaluateDifference(int exp1,int exp2) {
+        int difference = 0;
+      
+        return difference;
+    }
+
+    public int evaluateproduct(int exp1,int exp2) {
+        int product = 0;
+      
+        return product;
+    }
+
+    public int evaluateDivision(int exp1,int exp2) {
+        int division = 0;
+      
+        return division;
     }
 }
