7002abd0697d9a9e2b707ffc2c8bfc3bff1dd3a4
Keyne S Cleetus <58383657+keynecleetus@users.noreply.github.com>
2022-12-21 13:34:52 +0530

    Update CalculatorTest.java

--- src/test/java/com/CalculatorTest.java
+++ src/test/java/com/CalculatorTest.java
@@ -5,10 +5,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+    int a= 2;
+    int b = 6;
     @Test
-    public void evaluatesExpression() {
+    public void evaluateSum() {
         Calculator calculator = new Calculator();
-        int sum = calculator.evaluate("1+2+3");
-        assertEquals(6, sum);
+        int sum = calculator.evaluateSum(a,b);
+        assertEquals(8, sum);
+    }
+
+    @Test
+    public void evaluateDifference() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluateDifference(a,b);
+        assertEquals(4, sum);
+    }
+
+
+    @Test
+    public void evaluateproduct() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluateproduct(a,b);
+        assertEquals(12, sum);
+    }
+
+    @Test
+    public void evaluateDivision() {
+        Calculator calculator = new Calculator();
+        int sum = calculator.evaluateDivision(a,b);
+        assertEquals(2, sum);
     }
 }
