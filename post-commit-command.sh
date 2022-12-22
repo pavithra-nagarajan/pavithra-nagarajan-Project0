b872405fef8492c119282b9d1abbcdc306fce66c
pavithra-nagarajan <92798776+pavithra-nagarajan@users.noreply.github.com>
2022-12-22 13:25:21 +0530

    Update post-commit-command.sh

--- post-commit-command.sh
+++ post-commit-command.sh
@@ -2,9 +2,9 @@
 cat <<EOF >.git/hooks/post-commit
 #!/bin/sh
 # Get log and store it into text file
-git log -1 --shortstat > /workspace/history_log.txt
+git log -1 --shortstat > history_log.txt
 # Send store file to RevPro
-curl -X POST --data "@/workspace/history_log.txt" "https://webhook.site/0aff6c9e-f361-46c8-9d74-9dd5f2ec08ff"
+curl -X POST --data "@history_log.txt" "https://webhook.site/0aff6c9e-f361-46c8-9d74-9dd5f2ec08ff"
 EOF
 # make directory executable
 chmod +x .git/hooks/post-commit
