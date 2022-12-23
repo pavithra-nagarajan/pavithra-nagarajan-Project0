b497262618c5c83deab33c33312b326133843330
pavithra-nagarajan <92798776+pavithra-nagarajan@users.noreply.github.com>
2022-12-23 12:03:05 +0530

    Update post-commit-command.sh

--- post-commit-command.sh
+++ post-commit-command.sh
@@ -1,10 +1,10 @@
 cat <<EOF >.git/hooks/post-commit
 git log -1 --shortstat > history_log.txt
-curl -X POST --data "@history_log.txt" "	https://webhook.site/d33b1d37-4613-49a1-b52f-9098e340b17e"
+curl -X POST --data "@history_log.txt" "https://webhook.site/d33b1d37-4613-49a1-b52f-9098e340b17e"
 EOF
 chmod +x .git/hooks/post-commit
 cat <<EOF >.git/hooks/pre-commit
 echo "pavithra"
-curl -X POST --data "@test_log.txt" "	https://webhook.site/d33b1d37-4613-49a1-b52f-9098e340b17e"
+curl -X POST --data "@test_log.txt" "https://webhook.site/d33b1d37-4613-49a1-b52f-9098e340b17e"
 EOF
 chmod +x .git/hooks/pre-commit
