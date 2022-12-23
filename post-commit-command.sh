76ddaf8a5fc6616b17ad84d74a9b66d2638fd99f
pavithra-nagarajan <92798776+pavithra-nagarajan@users.noreply.github.com>
2022-12-23 11:58:17 +0530

    Update post-commit-command.sh

--- post-commit-command.sh
+++ post-commit-command.sh
@@ -2,3 +2,7 @@ cat <<EOF >.git/hooks/post-commit
 git log -1 --shortstat > history_log.txt
 EOF
 chmod +x .git/hooks/post-commit
+cat <<EOF >.git/hooks/pre-commit
+echo "pavithra"
+EOF
+chmod +x .git/hooks/pre-commit
